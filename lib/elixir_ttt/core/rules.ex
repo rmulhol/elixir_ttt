42e41dc5b0fe30559ae122311278fe2fba4f00be
Robert Mulholand <rmulholand@8thlight.com>
2015-12-11 10:10:09 -0600

    refactor rules

--- lib/elixir_ttt/core/rules.ex
+++ lib/elixir_ttt/core/rules.ex
@@ -1,23 +1,44 @@
 defmodule Rules do
   def game_over?(board) do
-    Enum.empty?(Board.empty_spaces(board)) or
-    game_won?(board)
+    no_empty_spaces?(board) or game_won?(board)
   end
 
   def player_wins?(board, player) do
-    Enum.any?(winning_combinations(board), fn(combo) ->
-      Enum.all?(combo, fn(cell) -> cell === player end)
-    end)
+    all_belong_to_this_player? = player_combination_matcher(player)
+    Enum.any?(winning_combinations(board), all_belong_to_this_player?)
   end
 
   def game_won?(board) do
-    Enum.any?(winning_combinations(board), fn(combo) ->
-      Enum.count(Enum.uniq(combo)) === 1 and
-      Enum.all?(combo, fn(cell) -> cell !== Board.empty_space end)
-    end)
+    Enum.any?(winning_combinations(board), &all_belong_to_one_player?/1)
   end
 
-  def winning_combinations(board) do
+  defp winning_combinations(board) do
     Board.rows(board) ++ Board.columns(board) ++ Board.diagonals(board)
+  end
+
+  defp no_empty_spaces?(board) do
+    Enum.empty?(Board.empty_spaces(board))
+  end
+
+  defp player_combination_matcher(player) do
+    fn(combination) ->
+      Enum.all?(combination, fn(cell) -> cell === player end)
+    end
+  end
+
+  defp all_belong_to_one_player?(combination) do
+    all_the_same?(combination) and none_empty?(combination)
+  end
+
+  defp all_the_same?(combination) do
+    Enum.count(Enum.uniq(combination)) === 1
+  end
+
+  defp none_empty?(combination) do
+    Enum.all?(combination, &not_empty?/1)
+  end
+
+  defp not_empty?(cell) do
+    cell !== Board.empty_space
   end
 end
--- test/elixir_ttt/core/rules_test.exs
+++ test/elixir_ttt/core/rules_test.exs
@@ -5,14 +5,14 @@ defmodule RulesTest do
     empty_board = TestHelper.setup_board([" ", " ", " ",
                                           " ", " ", " ",
                                           " ", " ", " "])
-    assert !Rules.game_over?(empty_board)
+    refute Rules.game_over?(empty_board)
   end
 
   test "game isn't over for board with available spaces" do
     incomplete_board = TestHelper.setup_board(["X", " ", " ",
                                                " ", "O", " ",
                                                " ", " ", "X"])
-    assert !Rules.game_over?(incomplete_board)
+    refute Rules.game_over?(incomplete_board)
   end
 
   test "game is over for board with no available spaces" do
@@ -33,22 +33,22 @@ defmodule RulesTest do
     empty_board = TestHelper.setup_board([" ", " ", " ",
                                           " ", " ", " ",
                                           " ", " ", " "])
-    assert !Rules.player_wins?(empty_board, "X")
+    refute Rules.player_wins?(empty_board, "X")
   end
 
   test "player isn't winner on tie game board" do
     tie_game_board = TestHelper.setup_board(["X", "O", "X",
                                              "X", "O", "X",
                                              "O", "X", "O"])
-    assert !Rules.player_wins?(tie_game_board, "X")
-    assert !Rules.player_wins?(tie_game_board, "O")
+    refute Rules.player_wins?(tie_game_board, "X")
+    refute Rules.player_wins?(tie_game_board, "O")
   end
 
   test "player isn't winner on lost board" do
     lost_board = TestHelper.setup_board(["X", "X", "X",
                                          "O", "O", " ",
                                          " ", " ", " "])
-    assert !Rules.player_wins?(lost_board, "O")
+    refute Rules.player_wins?(lost_board, "O")
   end
 
   test "player is winner on won board" do
@@ -59,32 +59,30 @@ defmodule RulesTest do
   end
 
   test "game not won on empty board" do
-    empty_board = TestHelper.setup_board([" ", " ", " ", " ", " ", " ", " ", " ", " "])
-    assert !Rules.game_won?(empty_board)
+    empty_board = TestHelper.setup_board([" ", " ", " ",
+                                          " ", " ", " ",
+                                          " ", " ", " "])
+    refute Rules.game_won?(empty_board)
   end
 
   test "game not won on tie game board" do
-    tie_game_board = TestHelper.setup_board(["X", "O", "X", "X", "O", "X", "O", "X", "O"])
-    assert !Rules.game_won?(tie_game_board)
+    tie_game_board = TestHelper.setup_board(["X", "O", "X",
+                                             "X", "O", "X",
+                                             "O", "X", "O"])
+    refute Rules.game_won?(tie_game_board)
   end
 
   test "game won with player 'X' win" do
-    won_board = TestHelper.setup_board(["X", "X", "X", "O", "O", " ", " ", " ", " "])
+    won_board = TestHelper.setup_board(["X", "X", "X",
+                                        "O", "O", " ",
+                                        " ", " ", " "])
     assert Rules.game_won?(won_board)
   end
 
   test "game won with player 'O' win" do
-    won_board = TestHelper.setup_board(["X", "X", " ", "O", "O", "O", " ", " ", " "])
+    won_board = TestHelper.setup_board(["X", "X", " ",
+                                        "O", "O", "O",
+                                        " ", " ", " "])
     assert Rules.game_won?(won_board)
-  end
-
-  test "winning combinations aggregates rows, columns, and diagonals" do
-    board = TestHelper.setup_board(["A", "B", "C",
-                                    "D", "E", "F",
-                                    "G", "H", "I"])
-    winning_combinations = [["A", "B", "C"], ["D", "E", "F"], ["G", "H", "I"],
-                            ["A", "D", "G"], ["B", "E", "H"], ["C", "F", "I"],
-                            ["A", "E", "I"], ["C", "E", "G"]]
-    assert Rules.winning_combinations(board) === winning_combinations
   end
 end
