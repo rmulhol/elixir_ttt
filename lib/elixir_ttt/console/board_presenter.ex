15bfdb7cda7498fd2f2f0c9f70b37186f2859f21
Robert Mulholand <rmulholand@8thlight.com>
2015-12-11 10:48:30 -0600

    refactor board presenter

--- lib/elixir_ttt/console/board_presenter.ex
+++ lib/elixir_ttt/console/board_presenter.ex
@@ -1,16 +1,36 @@
 defmodule BoardPresenter do
   def present(board) do
     rows = Board.rows(board)
-    Enum.map_join(rows, line_separator(rows), &present_row/1) <> "\n"
+    Enum.map_join(rows, line_separator(rows), &present_row/1) <> crlf
   end
 
-  def present_row(row) do
-    " " <> Enum.join(row, " | ") <> " "
+  defp present_row(row) do
+    space <> Enum.join(row, " | ") <> space
   end
 
-  def line_separator(rows) do
-    num_rows = Enum.count(rows)
-    num_dashes = num_rows * 3 + num_rows - 1
-    "\n" <> String.duplicate("-", num_dashes) <> "\n"
+  defp line_separator(rows) do
+    crlf <> String.duplicate("-", num_dashes(rows)) <> crlf
+  end
+
+  defp num_dashes(rows) do
+    num_cells = Enum.count(rows)
+    num_dashes_for_cells(num_cells) + num_dashes_for_boundaries(num_cells)
+  end
+
+  defp num_dashes_for_cells(num_cells) do
+    num_dashes_per_cell = 3
+    num_dashes_per_cell * num_cells
+  end
+
+  defp num_dashes_for_boundaries(num_cells) do
+    num_cells - 1
+  end
+
+  defp space do
+    " "
+  end
+
+  defp crlf do
+    "\n"
   end
 end
--- test/elixir_ttt/console/board_presenter_test.exs
+++ test/elixir_ttt/console/board_presenter_test.exs
@@ -54,28 +54,4 @@ defmodule BoardPresenterTest do
                             " X |   |   | O \n"
     assert BoardPresenter.present(board_4x4) === expected_presentation
   end
-
-  test "builds a 3x3 row" do
-    row_3x3 = ["X", "O", " "]
-    expected_presentation = " X | O |   "
-    assert BoardPresenter.present_row(row_3x3) === expected_presentation
-  end
-
-  test "builds a 4x4 row" do
-    row_4x4 = ["X", "O", " ", "X"]
-    expected_presentation = " X | O |   | X "
-    assert BoardPresenter.present_row(row_4x4) === expected_presentation
-  end
-
-  test "builds a line separator for a 3x3 board" do
-    rows_3x3 = Board.create(3) |> Board.rows
-    expected_presentation = "\n-----------\n"
-    assert BoardPresenter.line_separator(rows_3x3) === expected_presentation
-  end
-
-  test "builds a line separator for a 4x4 board" do
-    rows_4x4 = Board.create(4) |> Board.rows
-    expected_presentation = "\n---------------\n"
-    assert BoardPresenter.line_separator(rows_4x4) === expected_presentation
-  end
 end
