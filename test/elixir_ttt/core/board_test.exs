292f88a9c99737bf76e199c732a99dfee3d9502f
Robert Mulholand <rmulholand@8thlight.com>
2015-12-11 09:40:31 -0600

    add board setup to test helper
    - prevent brittleness if underlying data structure of board changes

--- test/elixir_ttt/core/board_test.exs
+++ test/elixir_ttt/core/board_test.exs
@@ -2,46 +2,46 @@ defmodule BoardTest do
   use ExUnit.Case
 
   test "creates an empty 3x3 board" do
-    board_3x3 = [" ", " ", " ",
-                 " ", " ", " ",
-                 " ", " ", " "]
+    board_3x3 = TestHelper.setup_board([" ", " ", " ",
+                                        " ", " ", " ",
+                                        " ", " ", " "])
     assert Board.create(3) === board_3x3
   end
 
   test "creates an empty 4x4 board" do
-    board_4x4 = [" ", " ", " ", " ",
-                 " ", " ", " ", " ",
-                 " ", " ", " ", " ",
-                 " ", " ", " ", " "]
+    board_4x4 = TestHelper.setup_board([" ", " ", " ", " ",
+                                        " ", " ", " ", " ",
+                                        " ", " ", " ", " ",
+                                        " ", " ", " ", " "])
     assert Board.create(4) === board_4x4
   end
 
 
   test "places a move on an empty board" do
-    empty_board = [" ", " ", " ",
-                   " ", " ", " ",
-                   " ", " ", " "]
-    board_with_move = ["X", " ", " ",
-                       " ", " ", " ",
-                       " ", " ", " "]
+    empty_board = TestHelper.setup_board([" ", " ", " ",
+                                          " ", " ", " ",
+                                          " ", " ", " "])
+    board_with_move = TestHelper.setup_board(["X", " ", " ",
+                                              " ", " ", " ",
+                                              " ", " ", " "])
     assert Board.place_move(empty_board, 0, "X") === board_with_move
   end
 
   test "places a move on a board with moves" do
-    board_with_moves = ["X", "O", " ",
-                        " ", " ", " ",
-                        " ", " ", " "]
-    expected_board = ["X", "O", "X",
-                      " ", " ", " ",
-                      " ", " ", " "]
+    board_with_moves = TestHelper.setup_board(["X", "O", " ",
+                                               " ", " ", " ",
+                                               " ", " ", " "])
+    expected_board = TestHelper.setup_board(["X", "O", "X",
+                                                      " ", " ", " ",
+                                                      " ", " ", " "])
     actual_board = Board.place_move(board_with_moves, 2, "X")
     assert actual_board === expected_board
   end
 
   test "empty_spaces returns indexes of all spaces on empty board" do
-    empty_board = [" ", " ", " ",
-                   " ", " ", " ",
-                   " ", " ", " "]
+    empty_board = TestHelper.setup_board([" ", " ", " ",
+                                          " ", " ", " ",
+                                          " ", " ", " "])
 
     all_spaces = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
@@ -49,9 +49,9 @@ defmodule BoardTest do
   end
 
   test "empty_spaces returns indexes of open spaces on board with moves" do
-    board_with_moves = ["X", "O", " ",
-                        " ", " ", " ",
-                        " ", " ", " "]
+    board_with_moves = TestHelper.setup_board(["X", "O", " ",
+                                               " ", " ", " ",
+                                               " ", " ", " "])
 
     empty_spaces = [2, 3, 4, 5, 6, 7, 8]
 
@@ -59,23 +59,23 @@ defmodule BoardTest do
   end
 
   test "space_empty? returns true for unclaimed space" do
-    board_with_moves = ["X", "O", " ",
-                        " ", " ", " ",
-                        " ", " ", " "]
+    board_with_moves = TestHelper.setup_board(["X", "O", " ",
+                                               " ", " ", " ",
+                                               " ", " ", " "])
     assert Board.space_empty?(board_with_moves, 2)
   end
 
   test "space_empty? returns false for claimed space" do
-    board_with_moves = ["X", "O", " ",
-                        " ", " ", " ",
-                        " ", " ", " "]
+    board_with_moves = TestHelper.setup_board(["X", "O", " ",
+                                               " ", " ", " ",
+                                               " ", " ", " "])
     refute Board.space_empty?(board_with_moves, 1)
   end
 
   test "returns rows' contents for 3x3 board" do
-    board_3x3 = ["A", "B", "C",
-                 "D", "E", "F",
-                 "G", "H", "I"]
+    board_3x3 = TestHelper.setup_board(["A", "B", "C",
+                                        "D", "E", "F",
+                                        "G", "H", "I"])
     rows_3x3 = [["A", "B", "C"],
                 ["D", "E", "F"],
                 ["G", "H", "I"]]
@@ -83,10 +83,10 @@ defmodule BoardTest do
   end
 
   test "returns rows' contents for 4x4 board" do
-    board_4x4 = ["A", "B", "C", "D",
-                 "E", "F", "G", "H",
-                 "I", "J", "K", "L",
-                 "M", "N", "O", "P"]
+    board_4x4 = TestHelper.setup_board(["A", "B", "C", "D",
+                                        "E", "F", "G", "H",
+                                        "I", "J", "K", "L",
+                                        "M", "N", "O", "P"])
     rows_4x4 = [["A", "B", "C", "D"],
                 ["E", "F", "G", "H"],
                 ["I", "J", "K", "L"],
@@ -95,9 +95,9 @@ defmodule BoardTest do
   end
 
   test "returns columns' contents for 3x3 board" do
-    board_3x3 = ["A", "B", "C",
-                 "D", "E", "F",
-                 "G", "H", "I"]
+    board_3x3 = TestHelper.setup_board(["A", "B", "C",
+                                        "D", "E", "F",
+                                        "G", "H", "I"])
     columns_3x3 = [["A", "D", "G"],
                    ["B", "E", "H"],
                    ["C", "F", "I"]]
@@ -105,10 +105,10 @@ defmodule BoardTest do
   end
 
   test "returns columns' contents for 4x4 board" do
-    board_4x4 = ["A", "B", "C", "D",
-                 "E", "F", "G", "H",
-                 "I", "J", "K", "L",
-                 "M", "N", "O", "P"]
+    board_4x4 = TestHelper.setup_board(["A", "B", "C", "D",
+                                        "E", "F", "G", "H",
+                                        "I", "J", "K", "L",
+                                        "M", "N", "O", "P"])
     columns_4x4 = [["A", "E", "I", "M"],
                    ["B", "F", "J", "N"],
                    ["C", "G", "K", "O"],
@@ -117,34 +117,34 @@ defmodule BoardTest do
   end
 
   test "returns diagonals' contents for 3x3 board" do
-    board_3x3 = ["A", "B", "C",
-                 "D", "E", "F",
-                 "G", "H", "I"]
+    board_3x3 = TestHelper.setup_board(["A", "B", "C",
+                                        "D", "E", "F",
+                                        "G", "H", "I"])
     diagonals_3x3 = [["A", "E", "I"], ["C", "E", "G"]]
     assert Board.diagonals(board_3x3) === diagonals_3x3
   end
 
   test "returns diagonals' contents for 4x4 board" do
-    board_4x4 = ["A", "B", "C", "D",
-                 "E", "F", "G", "H",
-                 "I", "J", "K", "L",
-                 "M", "N", "O", "P"]
+    board_4x4 = TestHelper.setup_board(["A", "B", "C", "D",
+                                        "E", "F", "G", "H",
+                                        "I", "J", "K", "L",
+                                        "M", "N", "O", "P"])
     diagonals_4x4 = [["A", "F", "K", "P"], ["D", "G", "J", "M"]]
     assert Board.diagonals(board_4x4) === diagonals_4x4
   end
 
   test "returns the side length for a 3x3 board" do
-    board_3x3 = [" ", " ", " ",
-                 " ", " ", " ",
-                 " ", " ", " "]
+    board_3x3 = TestHelper.setup_board([" ", " ", " ",
+                                        " ", " ", " ",
+                                        " ", " ", " "])
     assert Board.side_length(board_3x3) === 3
   end
 
   test "returns the side length for a 4x4 board" do
-    board_4x4 = [" ", " ", " ", " ",
-                 " ", " ", " ", " ",
-                 " ", " ", " ", " ",
-                 " ", " ", " ", " "]
+    board_4x4 = TestHelper.setup_board([" ", " ", " ", " ",
+                                        " ", " ", " ", " ",
+                                        " ", " ", " ", " ",
+                                        " ", " ", " ", " "])
     assert Board.side_length(board_4x4) === 4
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,22 @@
 ExUnit.start()
+
+defmodule TestHelper do
+  def setup_board(passed_board) do
+    board_length = Enum.count(passed_board)
+    side_length = board_length |> :math.sqrt |> round
+    new_board = Board.create(side_length)
+    do_setup_board(passed_board, new_board, board_length, 0)
+  end
+
+  def do_setup_board(passed_board, new_board, board_length, current_index) do
+    cond do
+      current_index >= board_length -> new_board
+      Enum.at(passed_board, current_index) === " " ->
+        do_setup_board(passed_board, new_board, board_length, current_index + 1)
+      true ->
+        move_at_index = Enum.at(passed_board, current_index)
+        updated_board = Board.place_move(new_board, current_index, move_at_index)
+        do_setup_board(passed_board, updated_board, board_length, current_index + 1)
+    end
+  end
+end
